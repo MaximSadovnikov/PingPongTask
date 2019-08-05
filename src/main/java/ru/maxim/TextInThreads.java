25c54755fa14933ba7f5b46ea8da9b6481548820
maksimsadovnikov <sadovnikov.maxim@gmail.com>
2019-08-05 10:24:14 +0300

    Finish TextInThreads

--- src/main/java/ru/maxim/TextInThreads.java
+++ src/main/java/ru/maxim/TextInThreads.java
@@ -2,27 +2,28 @@ package ru.maxim;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Queue;
 import java.util.Scanner;
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class TextInThreads {
     public static void main(String[] args) throws FileNotFoundException, InterruptedException {
         final ExecutorService readService = Executors.newFixedThreadPool(1);
         final ExecutorService writeService = Executors.newFixedThreadPool(5);
-        final SynchronousQueue<String> queue = new SynchronousQueue<>();
+        final Queue<String> queue = new ConcurrentLinkedQueue<>();
+        // write your path
         final String path = "/Users/maksimsadovnikov/IdeaProjects/PingPongTask" +
                 "/src/main/java/ru/maxim/Latin-Lipsum.txt";
-//        Thread readThread = new Thread(new ReadFromThread(queue, path));
-//        readThread.start();
-        readService.submit(new ReadFromThread(queue, path));
-        Thread.sleep(1000);
-//        while (!queue.isEmpty()) {
-//            writeService.submit(new WriteFromThread(queue));
-//        }
+        Scanner scanner = new Scanner(new File(path));
+        ReadFromThread reader = new ReadFromThread(queue, path);
+        WriteFromThread writer = new WriteFromThread(queue);
+        AtomicInteger i = new AtomicInteger(0);
+        readService.submit(reader);
+        while (queue.isEmpty()){Thread.sleep(1);}
+        while (!queue.isEmpty()) {
+            writeService.submit(writer);
+        }
         readService.shutdown();
         writeService.shutdown();
     }
@@ -31,42 +32,48 @@ public class TextInThreads {
 class ReadFromThread implements Runnable {
 
     private final Scanner scanner;
-    private SynchronousQueue<String> queue;
+    private Queue<String> queue;
+    public volatile Boolean isRunning;
 
-    public ReadFromThread(SynchronousQueue<String> queue, String path) throws FileNotFoundException {
+    public ReadFromThread(Queue<String> queue, String path) throws FileNotFoundException {
         this.queue = queue;
         this.scanner = new Scanner(new File(path));
+        this.isRunning = new Boolean(true);
     }
 
     @Override
     public void run() {
         while (scanner.hasNext()) {
-            try {
-                queue.put(scanner.next());
-                System.out.println(scanner.next());
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+            queue.add(scanner.next());
         }
+        try {
+            Thread.sleep(1);
+        } catch (InterruptedException e) {
+            return;
+        }
+        this.isRunning = new Boolean(false);
+        Thread.currentThread().interrupt();
     }
 }
 
 class WriteFromThread implements Runnable {
 
-    private SynchronousQueue<String> queue;
+    private Queue<String> queue;
     private final AtomicInteger index = new AtomicInteger(0);
 
-    public WriteFromThread(SynchronousQueue<String> queue) {
+    public WriteFromThread(Queue<String> queue) {
         this.queue = queue;
     }
 
     @Override
     public void run() {
-        index.incrementAndGet();
-        try {
-            System.out.println(queue.take() + "\t\t" + index);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+        if (queue.peek() != null) {
+            index.incrementAndGet();
+            System.out.format("%4d\t\t\t%s\t\t\t%s\n",
+                    (index.get()),
+                    Thread.currentThread().getName(),
+                    queue.poll());
+
         }
     }
 }
